515cc43a1159a0bc1132bc1d0dd65df76eea2120
cellenar26 <cellenar26@gmail.com>
2021-01-15 20:32:55 +0900

    first init

---
+++ src/main/java/com/awsbook/sbsb/AwsWithSbApplication.java
@@ -0,0 +1,13 @@
+package com.awsbook.sbsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWithSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsWithSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awsbook/sbsb/AwsWithSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsbook.sbsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsWithSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
