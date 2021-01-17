c6e161b9210d8273f05d981a8384bdd718251016
cellenar26 <cellenar26@gmail.com>
2021-01-17 16:42:05 +0900

    before 4 api

---
+++ src/main/java/com/awsbook/sbsb/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.awsbook.sbsb.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
