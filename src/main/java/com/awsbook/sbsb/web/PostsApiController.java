7ceb6f816ab959c1cd486f216f429824bf38c6b2
cellenar26 <cellenar26@gmail.com>
2021-01-18 13:04:47 +0900

    ~before local dating

---
+++ src/main/java/com/awsbook/sbsb/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.awsbook.sbsb.web;
+
+import com.awsbook.sbsb.service.posts.PostsService;
+import com.awsbook.sbsb.web.dto.PostsResponseDto;
+import com.awsbook.sbsb.web.dto.PostsSaveRequestDto;
+import com.awsbook.sbsb.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
