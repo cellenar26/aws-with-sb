b0c52c2dccb9851050456b9dbf2b93fb3f9eab1b
cellenar26 <cellenar26@gmail.com>
2021-01-19 22:07:41 +0900

    after google login

---
+++ src/main/java/com/awsbook/sbsb/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.awsbook.sbsb.config.auth.dto;
+
+import com.awsbook.sbsb.domain.User.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
