cdede1317f8f3cffec382800a3493a24d4e1fa0e
cellenar26 <cellenar26@gmail.com>
2021-01-20 15:26:12 +0900

    done ch5_spring security

---
+++ src/main/java/com/awsbook/sbsb/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.awsbook.sbsb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/main/java/com/awsbook/sbsb/web/IndexController.java
+++ src/main/java/com/awsbook/sbsb/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.awsbook.sbsb.web;
 
+import com.awsbook.sbsb.config.auth.LoginUser;
 import com.awsbook.sbsb.config.auth.dto.SessionUser;
 import com.awsbook.sbsb.service.posts.PostsService;
 import com.awsbook.sbsb.web.dto.PostsResponseDto;
@@ -19,10 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
